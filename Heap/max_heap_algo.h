c7ea620a5ba4ee89c48748b52a337bf36a02b075
Riddhi Narkar <62413157+grey-hat2003@users.noreply.github.com>
2022-01-19 12:00:20 +0530

    completed heap

--- Heap/max_heap_algo.h
+++ Heap/max_heap_algo.h
@@ -15,9 +15,9 @@ unsigned long long right(unsigned long long i)
     return (2*i)+1;
 }
 
-void max_heapify(int a[], int index)
+void max_heapify(int a[], int size, int index)
 {
-    int largest, size = sizeof(a)/sizeof(a[0]);
+    int largest;
     int l = left(index);
     int r = right(index);
 
@@ -36,7 +36,28 @@ void max_heapify(int a[], int index)
         a[index] = a[largest];
         a[largest] = temp;
 
-        max_heapify(a, largest);
+        max_heapify(a, size, largest);
     }
 
+}
+
+void build_max_heap(int a[], int size)
+{
+    for(int i = floor(size/2); i>=0; i--)
+        max_heapify(a, size, i);
+}
+
+void heapsort(int a[], int size)
+{
+    build_max_heap(a, size);
+
+    for(int i = size; i>0; i--)
+    {
+        int temp = a[0];
+        a[0] = a[i];
+        a[i] = a[0];
+
+        size -= 1;
+        max_heapify(a, size, 0);
+    }
 }
